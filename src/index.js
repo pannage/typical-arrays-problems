74aafab665c940c5b2a79b834318aa2940715554
Tatsiana <pannage@gmail.com>
2020-10-03 13:40:04 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array != undefined && array.length >0) {
+    return Math.min.apply(null, array);
+  } else {
+    return 0
+  }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array != undefined && array.length >0) {
+    return Math.max.apply(null, array);
+  } else {
+    return 0
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array != undefined && array.length >0) {
+    let sum = 0;
+    for (let i=0; i<array.length; i++) {
+      sum = sum + array[i]
+    }
+    let avg = sum/array.length
+    return avg
+    } else {
+      return 0
+  }
 }
